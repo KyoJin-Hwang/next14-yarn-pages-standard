c3a9e7e0708ab3a69b2b2b3ebd819ea3a206f53b
황교진 <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-07-09 11:26:01 +0900

    test : 분기 테스트 by owen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Get started by editing[test]&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
           <div>
